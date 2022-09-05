e49bf78d0d4b4c603de7c0c877203028426fd72f
Yuri <yuri_rjx@hotmail.com>
2022-09-04 23:54:21 -0300

    Fim do módulo 4

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -2,9 +2,13 @@ import styles from './Cardapio.module.scss';
 import { ReactComponent as Logo } from 'assets/logo.svg';
 import Buscador from './Buscador';
 import { useState } from 'react';
+import Filtros from './Filtros';
+import Ordenador from './Ordenador';
 
 export default function Cardapio() {
   const [busca, setBusca] = useState('');
+  const [filtro, setFiltro] = useState<number | null>(null);
+  const [ordenador, setOrdenador] = useState('');
   return (
     <main>
       <nav className={styles.menu}>
@@ -16,6 +20,10 @@ export default function Cardapio() {
       <section className={styles.cardapio}>
         <h3 className={styles.cardapio__titulo}>Cardápio</h3>
         <Buscador busca={busca} setBusca={setBusca} />
+        <div className={styles.cardapio__filtros}>
+          <Filtros filtro={filtro} setFiltro={setFiltro} />
+          <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
+        </div>
       </section>
     </main>
   );
